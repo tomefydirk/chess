7849386ace6d52b08007480bf73bcc61fa3221fa
tomefydirk <tomefydirk@yahoo.com>
2025-04-09 14:26:20 +0300

    empl loader

--- input/lecture/EmplLoader.java
+++ input/lecture/EmplLoader.java
@@ -2,8 +2,8 @@ package lecture;
 import fonction.*;
 public class EmplLoader {
     public static String[] decomp_block(String phrase,String regex){
-        String[] s=phrase.split(regex,3);
-        for(int i=0;i<3;i++){
+        String[] s=phrase.split(regex);
+        for(int i=0;i<s.length;i++){
             s[i]=s[i].trim();
             s[i]=s[i].replaceAll("\n", "");
         }
@@ -33,7 +33,7 @@ public class EmplLoader {
         return value.replaceAll("Piece :","").trim();
     }
     public static String position_as_string(String value){
-        return vlaue.replaceAll("Position :","").trim();
+        return value.replaceAll("Position :","").trim();
     }
     public static String[] element_piece(String value){
         String p_as_string=piece_as_string(value);
@@ -41,13 +41,18 @@ public class EmplLoader {
     } 
     public static int[] element_position(String value){
         String p_as_string=position_as_string(value);
-        return extraireNombres(value);
+        return extraireNombres(p_as_string);
     }
-    public static String[] main_separation(){
-        
+    public static String[] main_separator(String input){
+       String[] block=decomp_block(input.trim(),"-->");
+       return block;
     }
-    public static String[] separation_par_break(){
-
+    public static String[] separation_par_break(String input){
+       String[] block=decomp_block(input.trim(),"break;");
+       for(int i=0;i<block.length;i++){
+        block[i]=block[i].trim();
+       }
+       return block;
     }
    /*  public static Piece block_into_piece(String input){
         String[] value=input.split(";");
@@ -56,8 +61,8 @@ public class EmplLoader {
         return (new Piece('n',"famfj"));
     }*/
     public static void main(String[] args) {
-        String [] a=element_piece("Piece :{k,noir}");
-        for(int i=0;i<2;i++){
+        String [] a=separation_par_break("jamfja break; cazemfje break; ");
+        for(int i=0;i<a.length;i++){
             System.out.println("on a :"+ a[i]);
         }
   }
